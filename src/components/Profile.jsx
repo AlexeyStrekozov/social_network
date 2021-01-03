4b999d93503004d816889644ef7c984879b43269
Алексей Стрекозов <aleksejstrekozov@MacBook-Air-Aleksej.local>
2021-01-03 11:24:57 +0600

    start project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div className="content">
+      <div>
+        <img alt="test" src='https://i11.kanobu.ru/r/eda1e6ce20aa944260eec27c4fa991c5/1040x-/u.kanobu.ru/editor/images/87/745731e3-7b2b-416a-b14c-c082228c0675.jpg' />
+      </div>
+      <div>
+        ava+desc
+      </div>
+      <div>
+        My posts
+      <div>
+          New post
+      </div>
+      </div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  )
+}
+
+export default Profile
